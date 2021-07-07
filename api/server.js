2ef3694e0ce8517fffff3992f288d558f1d603ef
Sarah Guidry <sarahbethguidry@gmail.com>
2021-07-07 14:43:27 -0500

    implemented post request

--- api/server.js
+++ api/server.js
@@ -1,3 +1,26 @@
-// BUILD YOUR SERVER HERE
+const express = require('express')
+const User = require('./users/model')
 
-module.exports = {}; // EXPORT YOUR SERVER instead of {}
+const app = express()
+
+app.use(express.json())
+
+app.post('/api/users', (req, res) => {
+	//validate req.body 
+	if (!req.body.name || !req.body.bio) {
+		res.status(422).json({ message: 'name and bio are required' })
+	} else {
+		const { name, bio } = req.body
+		User.create({ name, bio })
+			.then(user => {
+				res.status(201).json(user)
+			})
+			.catch(err => {
+				res.status(500).json({
+					message: err.message
+				})
+			})
+	}
+})
+
+module.exports = app // EXPORT YOUR SERVER instead of {}
